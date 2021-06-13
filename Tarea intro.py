07fe975740df99516e695c29bf698cbd0b73acd2
JoaquinPBarrientos <jpbarrientos@miuandes.cl>
2021-06-13 19:16:27 -0400

    Se sube hasta la pregunta 5

--- Tarea intro.py
+++ Tarea intro.py
@@ -1,17 +1,22 @@
 # Prueba 2 simulación
-import random 
+import random
 import matplotlib.pyplot as plt
+import numpy as np
 
 random.seed(198896900)
 
 N = 10**5
 bits = []
-
 for i in range(N):
     bits.append(random.randint(0,1))
 
+bits = np.array(bits)
 
+ook = np.zeros((N,8))
 
-plt.plot(range(N),bits)
-plt.show()
-print(range(10))
+for i in range(len(bits)):
+    if bits[i] == 0:
+        ook[i] = np.array([0 for i in range(8)])
+    else:
+        ook[i] = np.array([1 for i in range(8)])
+ook
